9f7dbecdc36ded38430a32d1752a5a754ab8fcc1
Julian Jorgensen <Julian@goldenchai.com>
2017-01-05 13:27:46 -0600

    make subsites through gulp

--- gulp/tasks/move-subsite-files.js
+++ gulp/tasks/move-subsite-files.js
@@ -2,10 +2,10 @@ var gulp          = require('gulp');
 
 gulp.task('deploy-blog', function(done) {
   // move blog specific pages into root dir
-  return gulp.src(['_site/joyfullee.com/**/*']).pipe(gulp.dest('./_site'));
+  // return gulp.src(['_site/joyfullee.com/**/*']).pipe(gulp.dest('./_site'));
 });
 
 gulp.task('deploy-marketing', function(done) {
   // move marketing specific pages into root dir
-  return gulp.src(['_site/jocelynlee.ca/**/*']).pipe(gulp.dest('./_site'));
+  // return gulp.src(['_site/jocelynlee.ca/**/*']).pipe(gulp.dest('./_site'));
 });
