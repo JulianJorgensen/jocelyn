e50d5cfe19640755325b798235202d2011281e07
Julian Jorgensen <Julian@goldenchai.com>
2017-01-05 14:53:25 -0600

    show jocelynlee.ca specific content only

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,8 +4,8 @@ var sequence      = require('run-sequence');
 
 gulp.task('build', function(done) {
   if(isBlog) {
-    sequence('clean', 'jekyll-build', 'deploy-blog', done);
+    sequence('clean', 'jekyll-build', 'deploy-blog', 'clean-deploy-sites', done);
   } else {
-    sequence('clean', 'jekyll-build', 'deploy-marketing', done);
+    sequence('clean', 'jekyll-build', 'deploy-marketing', 'clean-deploy-sites', done);
   }
 });
--- gulp/tasks/deploy-subsite.js
+++ gulp/tasks/deploy-subsite.js
@@ -1,4 +1,11 @@
 var gulp          = require('gulp');
+var del           = require('del');
+
+
+gulp.task('clean-deploy-sites', function() {
+  del(['_site/jocelynlee.ca']);
+  del(['_site/joyfullee.com']);
+});
 
 gulp.task('deploy-blog', function() {
   // move blog specific pages into root dir
