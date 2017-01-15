3ad3ec0d7339f4a87db83bb2713bbc745b3eff99
Julian Jorgensen <Julian@goldenchai.com>
2017-01-15 15:16:23 -0600

    Merge Joyfullee into Jocelynlee

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,9 +3,6 @@ var isBlog        = require('../util/isBlog');
 var sequence      = require('run-sequence');
 
 gulp.task('build', function(done) {
-  if(isBlog) {
-    sequence('clean', 'jekyll-build', 'deploy-blog', 'clean-deploy-sites', done);
-  } else {
-    sequence('clean', 'jekyll-build', 'deploy-marketing', 'clean-deploy-sites', done);
+    sequence('clean', 'jekyll-build', done);
   }
 });
